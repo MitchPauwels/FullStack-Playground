896614a58ec4511f81be588054a87169069049ec
Mitch Pauwels <Mitch.Pauwels@hotmail.com>
2023-01-08 09:21:33 +0000

    added middleware

--- GetPost/app.js
+++ GetPost/app.js
@@ -1,24 +1,21 @@
 const express = require('express')
 const app = express()
 
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
-
-
-
-// app.get('/', (req,res) => {
-//     res.render('index')
-// })
-
-
+// Routes
 app.get('/tacos', (req,res) => {
     res.send('GET /tacos request')
 })
 
 app.post('/tacos', (req,res) => {
+    console.log(req.body);
     res.send('POST /tacos response')
 })
 
-
+// Listen
 app.listen(3000, () => {
     console.log('LISTENING ON PORT 3000');
 })
