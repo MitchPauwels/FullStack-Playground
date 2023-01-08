1c505f17dd3d96d7b5b20b187b4332e2f00da00d
Mitch Pauwels <Mitch.Pauwels@hotmail.com>
2023-01-08 10:53:07 +0000

    implemented ejs, comments

--- GetPost/app.js
+++ GetPost/app.js
@@ -1,11 +1,51 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 // Middleware
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+// Set up EJS as the template engine
+app.set('view engine', 'ejs');
+
+// Set the views directory
+app.set("views", path.join(__dirname, "views"));
+
+// Fake existing comments
+const comments = [
+{
+    username: 'Mitch',
+    comment: 'FIRST TRY!'
+},
+{
+    username: 'Danni',
+    comment: 'The Holidays Stress Me out!'
+},
+{
+    username: 'J9',
+    comment: 'GUYS! I GOT ENGAGED!'
+},
+{
+    username: 'Taheo',
+    comment: 'I need a girlfriend'
+},
+{
+    username: 'Ana',
+    comment: 'I Got the internship!!!!!'
+},
+]
+
+
+
 // Routes
+
+app.get('/comments', (req,res) => {
+    
+    res.render('comments/index', {comments})
+})
+
+//================================
 app.get('/tacos', (req,res) => {
     res.send('GET /tacos request')
 })
