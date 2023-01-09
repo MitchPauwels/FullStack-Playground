4c259a0bef0f5192052aac7996a92242520a296e
Mitch Pauwels <Mitch.Pauwels@hotmail.com>
2023-01-09 21:23:42 +0000

    init project

---
+++ J9/main.js
@@ -0,0 +1,29 @@
+
+const section = document.querySelector('section')
+
+function getPokemon(number) {
+  for(let i = 1; i <= number; i++){
+    const random = Math.floor(Math.random() * 20) +1
+
+    // fetch(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${random}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.sprites.front_default);
+      const image = data.sprites.front_default;
+      console.log(image);
+      const img = document.createElement('img')
+      const p = document.createElement('p')
+      p.textContent = data.name
+      img.src = data.sprites.front_default
+      section.appendChild(img)
+      section.appendChild(p)
+
+    })
+  }
+
+}
+
+  getPokemon(20)
+
+
