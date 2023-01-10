04cba911dfaf888ec48187aa56de54e0f17bd5a4
Mitch Pauwels <Mitch.Pauwels@hotmail.com>
2023-01-10 09:39:17 +0000

    fixed image problem

---
+++ vite-project/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+export default function Navbar() {
+    return(
+        <div>
+            <h1>HELOOOOOOOOOOOO</h1>
+            <img src='images/vite.svg' />
+            <img src='images/react-icon-small.png' />
+
+        </div>
+    )
+}
