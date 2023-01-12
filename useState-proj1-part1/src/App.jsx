c59aaf3166d301e44ad35b8828e418b0afac158a
Mitch Pauwels <Mitch.Pauwels@hotmail.com>
2023-01-12 13:39:51 +0000

    First Commit

---
+++ useState-proj1-part1/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="state">
+			<h1 className="state--title">Is state important to know?</h1>
+			<div className="state--value">
+				<h1>Yes</h1>
+			</div>
+		</div>
+	);
+}
+
+export default App;
