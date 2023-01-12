e35c68b32fcc2903e8b42bf54378dba3d3d1545f
Mitch Pauwels <Mitch.Pauwels@hotmail.com>
2023-01-12 14:17:08 +0000

    first commit

---
+++ useState-proj2-practice/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+export default function App() {
+  /**
+   * Challenge: Set up state to track our count (initial value is 0)
+   */
+  return (
+      <div className="counter">
+          <button className="counter--minus">–</button>
+          <div className="counter--count">
+              <h1>0</h1>
+          </div>
+          <button className="counter--plus">+</button>
+      </div>
+  )
+}
