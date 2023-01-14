773ffc3fdc14d334dedbd7c4f01ad132b832a4e5
Mitch Pauwels <Mitch.Pauwels@hotmail.com>
2023-01-14 14:15:16 +0000

    restructure

--- memeGenerator/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-export default function Header () {
-    return (
-        <div className="header">
-            <img className="header-logo" src='images/Troll-Face.svg' />
-            <h2 className="header-title">Meme Generator</h2>
-            <h4 className="header-subtitle">React Course - Project 3</h4>
-        </div>
-    )
-}
--- travel-journal/src/components/Navbar.jsx
+++
@@ -1,8 +0,0 @@
-export default function Navbar () {
-    return (
-        <div className="navbar">
-            <img className="navbar-logo" src="images/globe.svg" />
-            <h3 className="navbar-title">My Travel Journal</h3>
-        </div>
-    )
-}
